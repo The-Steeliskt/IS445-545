09ea8e63efd0c347820a11760cc77f292841f080
Jacob Maston <maston.jacob@gmail.com>
2017-10-24 15:31:33 -0700

    first

--- website_template/app.js
+++ website_template/app.js
@@ -1,4 +1,4 @@
-const message = prompt('Enter a message')
+const message = prompt('\n \n Please choose a numbered option: \n 1: Show messages \n 2: Add a message\n 3: Delete a message \n 0: Quit')
 
 // alert('Your input is: ' + message)
 const output = document.getElementById("output")
