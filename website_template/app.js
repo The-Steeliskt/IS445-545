4317d1b7ca870ac0d081f6aebcdc0da4acb288c9
Jacob Maston <Maston.jacob@gmail.com>
2017-10-28 12:30:38 -0700

    2.0

--- website_template/app.js
+++ website_template/app.js
@@ -1,10 +1,26 @@
-const message /* change to numInput*/= prompt('\n \n Please choose a numbered option: \n 1: Show messages \n 2: Add a message\n 3: Delete a message \n 0: Quit')
+const numInput = prompt('Please choose a numbered option: \n 1: Show messages \n 2: Add a message\n 3: Delete a message \n 0: Quit')
 
-/* If (numInput ===1)
-    const output =document.getElementById('output')
-    'The current messages are: \n 1: Message 01 \n 2: Message 02 \n 3: Message 03 \n 4: Message 04 \n 5: Message 05' */
+const arr = ['1: Message 01', '2: Message 02', '3: Message 03', '4: Message 04', '5: Message 05'];
+var text = '';
+var i;
+for (i = 0; i < arr.length; i++) {
+    text += arr[i] + "<br>";
+}
+document.getElementById("output").innerHTML = text;
 
-
-// alert('Your input is: ' + message)
-const output = document.getElementById("output")
-output.innerHTML = 'Your message is ' + message
+if (numInput == 1) {
+    document.getElementById("output").innerHTML = 'The current messages are:' + layout
+} else if (numInput == 2) {
+    const userInput = prompt('Please enter a new message:');
+    arr.push('6: ' + userInput);
+    document.getElementById('output').innerHTML = arr
+} else if (numInput == 3) {
+    const pop = prompt('Please select a message index to be deleted, 1-5'); // restrict input to 1-5, throw error message if wrong value is selected
+    var p = pop;
+    arr.splice(p, 1);
+    document.getElementById("output").innerHTML = arr;
+} else if (numInput == 0) {
+    document.getElementById("output").innerHTML = 'Goodbye'
+} else {
+    document.getElementById("output").innerHTML = 'Invalid input, please refresh this page and select a valid response'
+}
